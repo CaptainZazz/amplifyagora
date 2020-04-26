126c2577dd3b71677c09a3a3da308ac792ec1de9
Zazz <CaptainZazz@users.noreply.github.com>
2020-04-26 17:13:25 +0100

    "Using / Styling our PhotoPicker Component"

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -3,19 +3,24 @@ import React from "react";
 import { Form, Button, Input, Notification, Radio, Progress } from "element-react";
 import { PhotoPicker } from "aws-amplify-react";
 
+const initialState = {
+  description: '',
+  price: '',
+  shipped: false,
+  imagePreview: '',
+  image: ''
+};
+
 class NewProduct extends React.Component {
-  state = {
-    description: '',
-    price: 0,
-    shipped: false
-  };
+  state = { ...initialState };
 
   handleAddProduct = () => {
-    console.log('handleAddProduct');
+    console.log('handleAddProduct', this.state);
+    this.setState({ ...initialState });
   };
 
   render() {
-    const { shipped } = this.state;
+    const { description, price, image, shipped, imagePreview } = this.state;
     return (
       <div className="flex-canter">
         <h2 className="header">Add New Product</h2>
@@ -23,11 +28,11 @@ class NewProduct extends React.Component {
           <Form className="market-header">
 
           <Form.Item label="Add Product Description">
-              <Input type="text" icon="information" placeholder="Description" onChange={description => this.setState({ description })} />
+              <Input type="text" icon="information" placeholder="Description" value={description} onChange={description => this.setState({ description })} />
             </Form.Item>
 
             <Form.Item label="Set Product Price">
-              <Input type="number" icon="plus" placeholder="Price ($USD)" onChange={price => this.setState({ price })} />
+              <Input type="number" icon="plus" placeholder="Price ($USD)" value={price} onChange={price => this.setState({ price })} />
             </Form.Item>
 
             <Form.Item label="Is the Product Shipped or Emailed to the Customer?">
@@ -37,10 +42,17 @@ class NewProduct extends React.Component {
               </div>
             </Form.Item>
 
-            <PhotoPicker />
+            {imagePreview && <img className="image-preview" src={imagePreview} alt="Product Preview"></img>}
+            <PhotoPicker
+              title="Product Image"
+              preview="hidden"
+              onLoad={url => this.setState({ imagePreview: url })}
+              onPick={file => this.setState({ image: file })}
+              theme={photoPickerTheme}
+            />
 
             <Form.Item>
-              <Button type="primary" onClick={this.handleAddProduct}>Add Product</Button>
+              <Button type="primary" onClick={this.handleAddProduct} disabled={!image || !description || !price}>Add Product</Button>
             </Form.Item>
 
           </Form>
@@ -49,5 +61,29 @@ class NewProduct extends React.Component {
     )
   }
 }
+
+const photoPickerTheme = {
+  formContainer: {
+    margin: 0,
+    padding: '.8em'
+  },
+  formSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  sectionBody: {
+    margin: 0,
+    width: '250px'
+  },
+  sectionHeader: {
+    padding: '.2em',
+    color: 'var(--darkAmazonOrange)'
+  },
+  photoPickerButton: {
+    display: 'none'
+  }
+};
 
 export default NewProduct;
